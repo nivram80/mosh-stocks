aef9f9f7ba64764e8ca4c264a3f195a485fba3c5
nivram80 <nivram80@gmail.com>
2022-11-28 23:01:07 -0600

    allow user to delete an account

--- app/src/components/Accounts.svelte
+++ app/src/components/Accounts.svelte
@@ -20,19 +20,19 @@
     }
   }
 
-  const onDelete = async (id) => {
-    console.log('deleting account...')
-    // try {
-    //   await apiService('accountsDelete', {id: id});
-    //   getAccounts();
-    // } catch (err) {
-    //   console.log(err);
-    // }
+  const onDelete = async (a) => {
+    if (confirm(`Are you sure you want to delete the account: ${a.name}`)) {
+      try {
+        await apiService('accountsDelete', {id: a.id});
+        getAccounts();
+      } catch (err) {
+        console.log(err);
+      }
+    }
   }
 
   const onSelect = (a) => {
     selectedAccount = a;
-    console.log('selectedAccount: ', selectedAccount)
   }
 
   const onSubmit = async (e, edit = false) => {
@@ -69,21 +69,29 @@
     <h2>Edit Account</h2>
     <form on:submit|preventDefault={(e) => onSubmit(e, true)} bind:this={formEl} autocomplete="off">
       <label for="name">Account Name</label><br>
-      <input type="text" name="name" id="name" autocomplete="off" value={selectedAccount.name} /><br>
+      <input type="text" name="name" id="name" minlength="2" maxlength="30" required autocomplete="off" value={selectedAccount.name} /><br>
       <input type="hidden" name="id" value={selectedAccount.id} />
-      <button type="submit">Submit</button>
-    </form>
+      <div class="actions">
+        <button type="submit">Submit</button>
+        <button type="submit" class="danger" on:click={() => onDelete(selectedAccount)} on:keypress={() => onDelete(selectedAccount)}>Delete</button>
+      </div>
+  </form>
   {:else}
     <h2>Add Account</h2>
     <form on:submit|preventDefault={onSubmit} bind:this={formEl} autocomplete="off">
       <label for="name">Account Name</label><br>
-      <input type="text" name="name" id="name" autocomplete="off" /><br>
+      <input type="text" name="name" id="name" minlength="2" maxlength="30" required autocomplete="off" /><br>
       <button type="submit">Submit</button>
     </form>
   {/if}
 </div>
 
 <style> 
+  .actions {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
   button {
     margin-top: 4px;
   }
